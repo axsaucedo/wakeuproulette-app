ae7baab3a80924e4c5afcc9fcef8356dc5f443f1
Alejandro Saucedo <axsauze@gmail.com>
2014-02-23 01:12:03 +0000

    Mock skelleton of the skelleton... lol

--- platforms/android/assets/www/js/router.js
+++ platforms/android/assets/www/js/router.js
@@ -5,26 +5,38 @@ define(function (require) {
     var $ = require('jquery'),
         Backbone = require('backbone'),
         PageSlider = require('app/utils/pageslider'),
-        HomeView = require('app/views/HomeView'),
+        AlarmView = require('app/views/AlarmView'),
+        WakeUpView = require('app/views/WakeUpView'),
+        ProfileView = require('app/views/ProfileView'),
 
-        slider = new PageSlider($('body')),
+        slider = new PageSlider($('.app-content')),
 
-        homeView = new HomeView();
+        alarmView = new AlarmView(),
+        wakeUpView = new WakeUpView(),
+        profileView = new ProfileView();
 
     return Backbone.Router.extend({
 
         routes: {
-            "": "home",
-            "products/:id": "productDetails",
-            "alarm": "alarm",
+            "": "alarm",
             "wakeup": "wakeup",
-            "profile": "profile"
+            "profile": "profile",
+            "products/:id": "productDetails"
         },
 
-        home: function () {
-            homeView.delegateEvents();
-            $('body').append(homeView.$el);
-            homeView.$el.attr("class", "page page-center");
+        alarm: function () {
+            alarmView.delegateEvents();
+            slider.slidePage(alarmView.$el);
+        },
+
+        wakeup: function () {
+            wakeUpView.delegateEvents();
+            slider.slidePage(wakeUpView.$el);
+        },
+
+        profile: function () {
+            profileView.delegateEvents();
+            slider.slidePage(profileView.$el);
         },
 
         productDetails: function (id) {
@@ -36,21 +48,6 @@ define(function (require) {
                     }
                 });
             });
-        },
-
-        wakeup: function() {
-            require(["app/models/product", "app/views/ProductView"], function (models, ProductView) {
-                var product = new models.Product({id: 2});
-                product.fetch({
-                    success: function (data) {
-                        slider.slidePage(new ProductView({model: data}).$el);
-                    }
-                });
-            });  
-        }, 
-
-        profile: function() {
-
         }
 
     });
